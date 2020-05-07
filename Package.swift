53132699ecdf6ae5fb8ed0ed608745064dd577a1
Anton Dolzhenko <dolzhenko.tony@gmail.com>
2020-05-07 18:48:28 +0300

    Add package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+  let package = Package(
+     name: "SDWebImageFLPlugin",
+     products: [
+         .library(
+             name: "SDWebImageFLPlugin",
+             targets: ["SDWebImageFLPlugin"]),
+     ],
+     dependencies: [
+     ],
+     targets: [
+         .target(
+             name: "SDWebImageFLPlugin",
+             dependencies: []),
+     ]
+ )
